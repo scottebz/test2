8ade742359064f6dee4740fca620fd949f5adb07
skahht <scotte@outlook.com>
2020-10-16 22:04:21 +1000

    aaa

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Jason is a poo");
+        }
+    }
+}
